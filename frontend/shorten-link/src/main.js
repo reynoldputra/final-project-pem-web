d3ffbabc528c36189a757b1aa291929abb24b875
ahnafms <ahnafmusyaffa47@gmail.com>
2022-12-01 12:17:46 +0700

    setup tailwind

--- frontend/shorten-link/src/main.js
+++ frontend/shorten-link/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './style/globals.css'
 
 const app = createApp(App)
 
